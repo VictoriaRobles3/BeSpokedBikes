d84694fe53ccf0b0220b8cd1d1f71006e6f076a1
VictoriaRobles3 <traroblx@gmail.com>
2024-07-27 19:39:31 -0400

    Add buttons to home page, change display of results of database connection.

--- BeSpokedBikes/src/BeSpokedGUI.java
+++ BeSpokedBikes/src/BeSpokedGUI.java
@@ -6,6 +6,13 @@ public class BeSpokedGUI extends JFrame{
 
     private Connection connection;
     private JTextArea resultArea;
+    //Create buttons to be displayed on main page
+    private JButton salesEmployeesButton;
+    private JButton customersButton;
+    private JButton productsButton;
+    private JButton salesButton;
+    private JButton quarterlyReportButton;
+    private JButton createAsaleButton;
 
     public BeSpokedGUI(){
         components();
@@ -18,12 +25,38 @@ public class BeSpokedGUI extends JFrame{
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+        //initialize buttons
+        salesEmployeesButton = new JButton("Sales Employees");
+        customersButton = new JButton("Customers");
+        productsButton = new JButton("Products");
+        salesButton = new JButton("Sales");
+        quarterlyReportButton = new JButton("Quarterly Report");
+        createAsaleButton = new JButton("Create a Sale");
+
+        //Panel for buttons
+        JPanel buttoPanel = new JPanel(new GridLayout(3, 2, 10, 10));
+        buttoPanel.add(salesEmployeesButton);
+        buttoPanel.add(customersButton);
+        buttoPanel.add(productsButton);
+        buttoPanel.add(salesButton);
+        buttoPanel.add(quarterlyReportButton);
+        buttoPanel.add(createAsaleButton);
+
+        //Add new button panel to main panel
+        mainPanel.add(buttoPanel);
+
+        //space between buttons and results connection area
+        mainPanel.add(Box.createVerticalStrut(100));
+
         //Results area to display connection to DB
-        resultArea = new JTextArea(10, 50);
+        resultArea = new JTextArea(2, 30);
         resultArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(resultArea);
-        scrollPane.setBorder(BorderFactory.createTitledBorder("Results"));
-        mainPanel.add(scrollPane);
+        resultArea.setLineWrap(true);
+        resultArea.setWrapStyleWord(true);
+        resultArea.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("Database Connection Status"), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+        JPanel resultPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        resultPanel.add(resultArea);
+        mainPanel.add(resultPanel);
 
         // Set the main panel to the JFrame
         setLayout(new BorderLayout());
