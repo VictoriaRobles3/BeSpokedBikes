f4fdfc1bdd7b31d35cc6906b0a1e6d2b7d721d2b
VictoriaRobles3 <traroblx@gmail.com>
2024-07-29 06:28:44 -0400

    Products list format fixed.

--- BeSpokedBikes/src/Products.java
+++ BeSpokedBikes/src/Products.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
-import java.time.*;
 
 public class Products extends JFrame {
     private Connection connection;
@@ -39,8 +38,8 @@ public class Products extends JFrame {
         ResultSet myRS = myStmt.executeQuery(sqlCommand)){
             StringBuilder text = new StringBuilder();
 
-            text.append("Name\t\tManufacturer\tStyle\t\tPurchase Price\t\tSale Price\tQuantity\tCommission Percentage\n");
-            text.append("-------------\t-----------\t------------\t-------------\t\t----------\t--------\t---------------------\n");
+            text.append("\nName\t\tManufacturer\tStyle\tPurchase Price\t\tSale Price\tQuantity\tCommission\n");
+            text.append("-------------\t-----------\t---------\t-----------\t\t--------\t-------\t----------\n");
 
             while (myRS.next()) {
                 String name = myRS.getString("name");
@@ -51,7 +50,7 @@ public class Products extends JFrame {
                 int qtyOnHand = myRS.getInt("QtyOnHand");
                 Double commissionPercentage = myRS.getDouble("commissionPercentage");
 
-                text.append(String.format("%-16s\t%-15s\t%-10s\t$%-13.2f\t\t$%-10.2f\t%-8d\t%.2f%%\n",
+                text.append(String.format("%-20s\t%-15s\t%-10s\t$%-13.2f\t\t$%-10.2f\t%-8d\t$%.2f\n",
                 name,
                 manufacturer,
                 prodStyle,
